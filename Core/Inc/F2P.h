eaa4a3dcb93cb7450d7f80b6aa746c73d91dda5b
Oriol <oriolcasanovag@gmail.com>
2026-02-27 17:00:28 +0100

    Nou .c

---
+++ Core/Inc/F2P.h
@@ -0,0 +1,16 @@
+/*
+ * F2P.h
+ *
+ *  Created on: Feb 27, 2026
+ *      Author: oriol
+ */
+
+#ifndef INC_F2P_H_
+#define INC_F2P_H_
+
+#include "main.h" // Para tener acceso a las librerías HAL
+
+// Prototipos de tus funciones
+void F2P_Process(void);
+
+#endif /* INC_F2P_H_ */
---
+++ Core/Src/F2P.c
@@ -0,0 +1,12 @@
+/*
+ * F2P.c
+ *
+ *  Created on: Feb 27, 2026
+ *      Author: oriol
+ */
+#include "F2P.h"
+
+void F2P_Process(void) {
+    // Tu lógica aquí
+}
+
